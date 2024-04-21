b8ee94fbed5aa1a6997318ef316cabc234eecc2f
JLozanoDG <jlozano@lifeisthegame.email>
2024-04-21 18:46:07 -0500

    Javier: [Add] Firestore / Firebase Linked

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js'
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCmUnyjSh2z2qTz2aCQ-NuA22C9QOGy0p4",
+    authDomain: "aaco-d7340.firebaseapp.com",
+    projectId: "aaco-d7340",
+    storageBucket: "aaco-d7340.appspot.com",
+    messagingSenderId: "658480347902",
+    appId: "1:658480347902:web:8a9764445579dd7f0ec3af",
+    measurementId: "G-MBDEFL6D79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "news"));
+querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+});
+
+

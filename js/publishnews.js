adfd59b89a6c495d6f7baf9763aeebc3fd8a6647
JLozanoDG <jlozano@lifeisthegame.email>
2024-05-01 19:22:25 -0500

    Javier: [Add] Publish News modal + function

---
+++ js/publishnews.js
@@ -0,0 +1,56 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp} from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
+import { showMessage } from "./showMessage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmUnyjSh2z2qTz2aCQ-NuA22C9QOGy0p4",
+    authDomain: "aaco-d7340.firebaseapp.com",
+    projectId: "aaco-d7340",
+    storageBucket: "aaco-d7340.appspot.com",
+    messagingSenderId: "658480347902",
+    appId: "1:658480347902:web:8a9764445579dd7f0ec3af",
+    measurementId: "G-MBDEFL6D79"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const colNews = collection(db, 'news');
+const newsform = document.querySelector("#publish-news-form");
+const storage = getStorage();
+
+newsform.addEventListener('submit', async e => {
+    
+    e.preventDefault()
+
+    const title = newsform['news-title'].value;
+    const description = newsform['news-description'].value;
+    const text = newsform['news-text'].value;
+    let splittext = text.split('\n\n');
+    const imagefile = newsform['news-image'].files[0];
+    const storageRef = ref(storage, title);
+    const linkimage = "";
+    const modal = bootstrap.Modal.getInstance(document.querySelector('#modalnews'));
+
+    showMessage("Cargando...", "success");
+
+    try{
+
+        const uploadfile = await uploadBytes(storageRef, imagefile);
+
+        const geturl = await getDownloadURL(storageRef);
+
+        const newNews = await addDoc(colNews, {
+          title: title,
+          description: description,
+          date: serverTimestamp(),
+          news: splittext,
+          image: geturl,
+        });
+        modal.hide();
+        showMessage("Nueva noticia publicada", "success");
+
+    } catch{
+        showMessage("Ocurrio un problema, intente nuevamente");
+    }
+})
